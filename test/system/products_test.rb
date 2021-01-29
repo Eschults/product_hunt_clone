fb176cc0938c726d9e71970ebabdb9065a3d9dc8
Edward Schults <edward.schults@gmail.com>
2021-01-29 13:08:20 +0100

    comment out 2nd test

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -3,7 +3,6 @@ require "application_system_test_case"
 class ProductTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit "/"
-    save_and_open_screenshot
     assert_selector "h1", text: "Awesome Products"
   end
 
